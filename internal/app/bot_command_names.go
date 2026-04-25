766a495d5f231a6ec847610b9afe8fe4feb4042e
normiridium <normiridium@gmail.com>
2026-04-25 19:51:42 +0000

    centralize bot command names into constants

---
+++ internal/app/bot_command_names.go
@@ -0,0 +1,25 @@
+package app
+
+const (
+	cmdStart             = "start"
+	cmdHelp              = "help"
+	cmdEmojiID           = "emojiid"
+	cmdEmojiIDAlias      = "emoji_id"
+	cmdStickerID         = "stickerid"
+	cmdStickerIDAlias    = "sticker_id"
+	cmdSpotifySearch     = "spsearch"
+	cmdSpotifySearchAlt  = "spfind"
+	cmdMyPortrait        = "my_portrait"
+	cmdMyPortraitAlias   = "portrait"
+	cmdDeleteMyPortrait  = "delete_my_portrait"
+	cmdDeleteMyPortrait2 = "clear_my_portrait"
+
+	cmdBan         = "ban"
+	cmdUnban       = "unban"
+	cmdMute        = "mute"
+	cmdUnmute      = "unmute"
+	cmdKick        = "kick"
+	cmdReadonly    = "readonly"
+	cmdReloadAdmins = "reload_admins"
+)
+
